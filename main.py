80e142519f5908021137200d66a8e15492db1d9b
praveenkumar <114279329+praveenkumarsuresh5201@users.noreply.github.com>
2025-07-07 20:15:42 +0530

    leatest commit main.py

--- main.py
+++ main.py
@@ -320,4 +320,4 @@ async def api_status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
